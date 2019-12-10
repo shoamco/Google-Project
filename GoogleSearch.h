9661b6f06767025ed49ac603f80c0f398b847447
shoamco <shoamc98@gmail.com>
2019-12-10 05:32:14 -0800

    return to the user vector of completion

--- GoogleSearch.h
+++ GoogleSearch.h
@@ -8,14 +8,17 @@
 #include <vector>
 #include <string>
 #include "AutoCompleteData.h"
-
+#include "OffLineData.h"
 
 class GoogleSearch{
 public:
+
     std::vector<AutoCompleteData> GetBestKCompletions(const std::string& prefix);
     void Run();
+    OffLineData offLineData;
 
 };
+
 inline void GoogleSearch::Run() {
     /**
     The function waiting for user input  ,get input and  printing  the best  5 complement * */
@@ -24,10 +27,11 @@ inline void GoogleSearch::Run() {
         std::cout << "enter search\n ";
         std::getline (std::cin,input);//get input user
         std::cout<<"input "<<input<<std::endl;
-//        std::vector<AutoCompleteData> best_completions= GetBestKCompletions(input);
+        std::vector<AutoCompleteData> best_completions= GetBestKCompletions(input);
+        printVectorCompletion(best_completions);
     }
 }
-//inline std::vector<AutoCompleteData> GetBestKCompletions(const std::string& prefix){
-//
-//}
+inline std::vector<AutoCompleteData> GoogleSearch::GetBestKCompletions(const std::string& prefix){
+    return offLineData.GetVectorCompletion(prefix);
+}
 #endif //AUTOCOMPLETEGOOGLE_GOOGLESEARCH_H
