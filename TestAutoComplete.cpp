db5efc6ef3ab5f05f866908787a9e0bff96f7f87
shoamco <shoamc98@gmail.com>
2019-12-10 04:16:17 -0800

    add testing

---
+++ TestAutoComplete.cpp
@@ -0,0 +1,24 @@
+//
+// Created by shoam on 12/10/19.
+//
+
+#include "TestAutoComplete.h"
+//*********** GOOGLE TEST************
+//TEST(AutoCompleteDataTests, ctor) {
+//
+//}
+/***************   Test Class GoogleSearch  *************/
+void testRunSearch(){
+    std::cout << "\n******test run search *****\n\n" << std::endl;
+    GoogleSearch googleSearch;
+    googleSearch.Run();
+}
+/***************  Test Class   GoogleSearch  *************/
+void testCreateAutoCompleteData(){
+//    AutoCompleteData autoCompleteData
+
+}
+/***************  Test Class  OffLineData  *************/
+void test_init_offline_data(){
+
+}
---
+++ TestAutoComplete.h
@@ -0,0 +1,15 @@
+
+
+#ifndef AUTOCOMPLETEGOOGLE_TESTAUTOCOMPLETE_H
+#define AUTOCOMPLETEGOOGLE_TESTAUTOCOMPLETE_H
+
+//#include <gtest/gtest.h>
+#include <iostream>
+#include "GoogleSearch.h"
+#include "AutoCompleteData.h"
+#include "OffLineData.h"
+#include <string>
+
+void testRunSearch();
+void testCreateAutoCompleteData();
+#endif //AUTOCOMPLETEGOOGLE_TESTAUTOCOMPLETE_H
