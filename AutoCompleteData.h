04148513887bb71c0b27ce9d60eb67ee6a065857
shoamco <shoamc98@gmail.com>
2019-12-10 06:19:25 -0800

    add gets function

--- AutoCompleteData.h
+++ AutoCompleteData.h
@@ -9,33 +9,45 @@
 
 class AutoCompleteData {
 public:
-    AutoCompleteData(std::string completed_sentence, std::string source_text, int offset);
+    AutoCompleteData(std::string source_text, int offset, std::string completed_sentence);
 
     int CalculateSource();
+
     friend std::ostream &operator<<(std::ostream &ostream, const AutoCompleteData autoCompleteData);
 
+    std::string GetCompletedSentence();
+
 private:
-    std::string completed_sentence;
     std::string source_text;
     int offset;
+    std::string completed_sentence;
     int score;
 
 };
-inline int AutoCompleteData::CalculateSource(){
+
+inline std::string AutoCompleteData::GetCompletedSentence() {
+    return completed_sentence;
+
+}
+
+inline int AutoCompleteData::CalculateSource() {
     return 1;
 }
-inline   AutoCompleteData::AutoCompleteData(std::string completed_sentence, std::string source_text, int offset)
-        : completed_sentence(completed_sentence), source_text(source_text), offset(offset) {
-    score=CalculateSource();
+
+inline   AutoCompleteData::AutoCompleteData(std::string source_text, int offset, std::string completed_sentence)
+        : source_text(source_text), offset(offset), completed_sentence(completed_sentence) {
+    score = CalculateSource();
 
 }
-inline std::ostream &operator<<(std::ostream &ostream, const AutoCompleteData autoCompleteData){
-    ostream << "AutoCompleteData: \n";
-    ostream<<"completed_sentence: "<<autoCompleteData.completed_sentence;
-    ostream<<"\nsource_text: "<<autoCompleteData.source_text;
-    ostream<<"\noffset: "<<autoCompleteData.offset;
-    ostream<<"\nscore: "<<autoCompleteData.score;
 
-    return  ostream <<std::endl;
+inline std::ostream &operator<<(std::ostream &ostream, const AutoCompleteData autoCompleteData) {
+    ostream << "\n\nAutoCompleteData: \n";
+    ostream << "source_text: " << autoCompleteData.source_text;
+    ostream << "\noffset: " << autoCompleteData.offset;
+    ostream << "\ncompleted_sentence: " << autoCompleteData.completed_sentence;
+    ostream << "\nscore: " << autoCompleteData.score;
+
+    return ostream << std::endl;
 }
+
 #endif //AUTOCOMPLETEGOOGLE_AUTOCOMPLETEDATA_H
