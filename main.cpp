69add3c6bab9b0114aacedaff7e56a65870115ec
shoamco <shoamc98@gmail.com>
2019-12-10 04:15:57 -0800

    add main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "TestAutoComplete.h"
+int main() {
+testRunSearch();
+
+    return 0;
+}
+
+//int main(int argc, char *argv[]) {
+//
+//    testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
