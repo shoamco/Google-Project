4c06a2fbfa66d8216412a247fafc45f169ce19c1
shoamco <shoamc98@gmail.com>
2019-12-10 04:38:48 -0800

    add ctor and ostream operator

--- AutoCompleteData.h
+++ AutoCompleteData.h
@@ -9,9 +9,10 @@
 
 class AutoCompleteData {
 public:
-    AutoCompleteData(std::string completed_sentence, std::string source_text);
+    AutoCompleteData(std::string completed_sentence, std::string source_text, int offset);
 
-//    int CalculateSource();
+    int CalculateSource();
+    friend std::ostream &operator<<(std::ostream &ostream, const AutoCompleteData autoCompleteData);
 
 private:
     std::string completed_sentence;
@@ -20,10 +21,21 @@ private:
     int score;
 
 };
-
-inline   AutoCompleteData::AutoCompleteData(std::string completed_sentence, std::string source_text)
-        : completed_sentence(completed_sentence), source_text(source_text) {
+inline int AutoCompleteData::CalculateSource(){
+    return 1;
+}
+inline   AutoCompleteData::AutoCompleteData(std::string completed_sentence, std::string source_text, int offset)
+        : completed_sentence(completed_sentence), source_text(source_text), offset(offset) {
+    score=CalculateSource();
 
 }
+inline std::ostream &operator<<(std::ostream &ostream, const AutoCompleteData autoCompleteData){
+    ostream << "AutoCompleteData: \n";
+    ostream<<"completed_sentence: "<<autoCompleteData.completed_sentence;
+    ostream<<"\nsource_text: "<<autoCompleteData.source_text;
+    ostream<<"\noffset: "<<autoCompleteData.offset;
+    ostream<<"\nscore: "<<autoCompleteData.score;
 
+    return  ostream <<std::endl;
+}
 #endif //AUTOCOMPLETEGOOGLE_AUTOCOMPLETEDATA_H
