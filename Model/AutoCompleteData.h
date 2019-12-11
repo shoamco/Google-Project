3d3b9a3dfa2917329485bae2f20b53486614a604
shoamco <shoamc98@gmail.com>
2019-12-11 05:00:28 -0800

    remove memeber completed_sentence(hold only offset to completed_sentence

--- Model/AutoCompleteData.h
+++ Model/AutoCompleteData.h
@@ -7,40 +7,42 @@
 
 #include <string>
 #include<string.h>
+
 class AutoCompleteData {
 public:
-    AutoCompleteData(std::string source_text, int offset, std::string completed_sentence,int score);
+    AutoCompleteData(std::string source_text, size_t offset, int score);
 
     int CalculateSource();
 
     friend std::ostream &operator<<(std::ostream &ostream, const AutoCompleteData autoCompleteData);
 
-    std::string GetCompletedSentence();
+    size_t GetOffset();
+
+//    size_t GetCompletedSentenceLine();
+
     int GetScore();
 
 private:
     std::string source_text;
-    int offset;
-    std::string completed_sentence;
+    size_t offset;
     int score;
 
 };
-
-inline std::string AutoCompleteData::GetCompletedSentence() {
-    return completed_sentence;
-
+inline size_t AutoCompleteData::GetOffset(){
+    return offset;
 }
+//inline int AutoCompleteData::GetCompletedSentenceLine() {
+//    return completed_sentence;
+//
+//}
 inline int AutoCompleteData::GetScore() {
     return score;
 
 }
 
-inline int AutoCompleteData::CalculateSource() {
-    return strlen(completed_sentence.c_str())*2 ;
-}
 
-inline   AutoCompleteData::AutoCompleteData(std::string source_text, int offset, std::string completed_sentence,int score)
-        : source_text(source_text), offset(offset), completed_sentence(completed_sentence),score(score) {
+inline   AutoCompleteData::AutoCompleteData(std::string source_text, size_t offset, int score)
+        : source_text(source_text), offset(offset), score(score) {
 
 
 }
@@ -49,7 +51,7 @@ inline std::ostream &operator<<(std::ostream &ostream, const AutoCompleteData au
     ostream << "\n\nAutoCompleteData: \n";
     ostream << "source_text: " << autoCompleteData.source_text;
     ostream << "\noffset: " << autoCompleteData.offset;
-    ostream << "\ncompleted_sentence: " << autoCompleteData.completed_sentence;
+//    ostream << "\ncompleted_sentence: " << autoCompleteData.completed_sentence;
     ostream << "\nscore: " << autoCompleteData.score;
 
     return ostream << std::endl;
