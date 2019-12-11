3c546c04b5c760016992901500f6510440452260
shoamco <shoamc98@gmail.com>
2019-12-11 04:56:34 -0800

    add function that show all completion to the user

--- View/CompletionView.h
+++ View/CompletionView.h
@@ -5,4 +5,21 @@
 #ifndef GOOGLE_PROJECT_SHOAMCO_COMPLETIONVIEW_H
 #define GOOGLE_PROJECT_SHOAMCO_COMPLETIONVIEW_H
 
+#include "../Model/OffLineData.h"
+
+class CompletionView{
+public:
+     void printToUser(VectorCompletion best_completions,OffLineData offLineData);
+//     void printToUser(VectorCompletion best_completions);
+};
+
+inline void CompletionView::printToUser(VectorCompletion vec,OffLineData offLineData) {
+    for (int i = 0; i < vec.size(); i++) {
+
+        size_t index=vec.at(i).GetOffset();
+        std::cout <<"(line:"<<index<<") "<< offLineData.getLineByOffset(index)<<std::endl;
+
+    }
+}
+
 #endif //GOOGLE_PROJECT_SHOAMCO_COMPLETIONVIEW_H
