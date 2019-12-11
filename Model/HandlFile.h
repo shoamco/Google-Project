6c57b0f2057fa090ea84700ce954f9c5c9762d3c
shoamco <shoamc98@gmail.com>
2019-12-11 02:28:55 -0800

    add class to handl file

--- Model/HandlFile.h
+++ Model/HandlFile.h
@@ -5,4 +5,28 @@
 #ifndef GOOGLE_PROJECT_SHOAMCO_HANDLFILE_H
 #define GOOGLE_PROJECT_SHOAMCO_HANDLFILE_H
 
+#include <vector>
+#include <string>
+class HandleFile{
+
+
+public:
+    std::vector<std::string> FileToArray(std::string file_name);
+};
+
+
+
+
+inline  std::vector<std::string> HandleFile::FileToArray(std::string file_name){
+    std::ifstream file(file_name);
+//    ifstream file("../../source_text.txt");
+    std::vector<std::string> lines;
+    if(file.is_open())
+    {
+
+
+
+    }
+    return lines;
+}
 #endif //GOOGLE_PROJECT_SHOAMCO_HANDLFILE_H
