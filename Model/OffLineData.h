65ec4151fcfd4f445fb65113cd4a55d488b7aad5
shoamco <shoamc98@gmail.com>
2019-12-11 05:02:37 -0800

    add vector that hold all lines and function to get direct access to a line (by offset)

--- Model/OffLineData.h
+++ Model/OffLineData.h
@@ -16,6 +16,7 @@
 
 using json = nlohmann::json;
 typedef std::vector<AutoCompleteData> VectorCompletion;
+typedef std::vector<std::string> VectorString;
 typedef std::unordered_map<std::string, VectorCompletion> MapComplete;
 
 /**
@@ -26,51 +27,28 @@ typedef std::unordered_map<std::string, VectorCompletion> MapComplete;
  * */
 class OffLineData {
 public:
-    OffLineData(MapComplete offline_table);
+    OffLineData(MapComplete offline_table,VectorString all_source_text);
 
     MapComplete GetMap() const;
 
     VectorCompletion GetVectorCompletion(const std::string key);
 
     int *GetAllCompletion(const std::string key);
+    std::string getLineByOffset(int offset);
 
 private:
     MapComplete offline_table;
+     VectorString lines;
+
 //    json offline_data;
 
 };
-inline json openFileToJson(std::string file_name){
+inline std::string OffLineData::getLineByOffset(int offset){
+    return lines[offset-1];
 
 }
-inline OffLineData::OffLineData( MapComplete mapComplete):offline_table(mapComplete) {
-//    int score;
-//    std::string completed_sentence;
-//    std::string source_text;
-//    json offline_data;
-//    std::string file_name("../test.json");
-//    std::ifstream file(file_name);
-//    if (file.is_open()) {
-//        offline_data = json::parse(file);
-//
-//        for (json::iterator it = offline_data.begin(); it != offline_data.end(); ++it) {
-//          completed_sentence=it.key();
-//
-//            score = it.value()[1];
-//            source_text = it.value()[0];
-//            int x = it.value()[2][0];
-//            int line;
-//
-//            VectorCompletion vectorComplete;
-//            for (int i = 0; i < it.value()[2].size(); ++i) {//enter to vector  all completed sentence
-//                line = it.value()[2][i];
-//                AutoCompleteData autoCompleteData(source_text,line,completed_sentence,score);
-//                vectorComplete.push_back(autoCompleteData);
-//            }
-//
-//            std::pair<std::string,VectorCompletion> pair_data (completed_sentence,vectorComplete);
-//            offline_table.insert(pair_data);
-//        }
-//    } else { throw "Unable to open file"; }
+
+inline OffLineData::OffLineData( MapComplete mapComplete,VectorString lines):offline_table(mapComplete),lines(lines) {
 
 
 }
@@ -94,17 +72,15 @@ inline void printVectorCompletion(VectorCompletion vec) {
 }
 
 inline void printAllCompletion(VectorCompletion vec) {
-    for (int i = 0; i < vec.size(); i++) {
-//        std::cout << vec.at(i).GetCompletedSentence()<<"\n\n";
-        std::cout << vec.at(i).GetCompletedSentence() << " (score: " << vec.at(i).GetScore() << " )" << "\n\n";
-    }
+//    for (int i = 0; i < vec.size(); i++) {
+////        std::cout << vec.at(i).GetCompletedSentence()<<"\n\n";
+//        std::cout << getLineByOffset(vec.at(i).GetOffset()) << " (score: " << vec.at(i).GetScore() << " )" << "\n\n";
+//    }
 }
 
 inline void printOfflineData(json offline_data) {
     for (json::iterator it = offline_data.begin(); it != offline_data.end(); ++it) {
-//          offline_table.insert ( std::make_pair<std::string,double>>(it.key(),it.value()))
-        std::cout << it.key() << " : " << it.value() << std::endl;
-        std::cout << it.key() << " : " << it.value() << std::endl;
+         std::cout << it.key() << " : " << it.value() << std::endl;
     }
 
 }
