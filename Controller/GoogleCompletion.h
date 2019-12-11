ae069c7a702d9241c1ba39eb5348be46bf176e0d
shoamco <shoamc98@gmail.com>
2019-12-11 04:54:18 -0800

    controller call to view to show all completion

--- Controller/GoogleCompletion.h
+++ Controller/GoogleCompletion.h
@@ -5,30 +5,38 @@
 #ifndef AUTOCOMPLETEGOOGLE_GOOGLESEARCH_H
 #define AUTOCOMPLETEGOOGLE_GOOGLESEARCH_H
 
+#include "../View/CompletionView.h"
 #include <vector>
 #include <string>
 #include "Model/AutoCompleteData.h"
 #include "Model/OffLineData.h"
-
+/**
+ GoogleCompletion
+ * */
 class GoogleCompletion{
 public:
-
+    GoogleCompletion( OffLineData offLineData);
     std::vector<AutoCompleteData> GetBestKCompletions(const std::string& prefix);
     void Run();
     OffLineData offLineData;
 
 };
+inline   GoogleCompletion::GoogleCompletion( OffLineData offLineData):offLineData(offLineData){
 
+}
 inline void GoogleCompletion::Run() {
     /**
     The function waiting for user input  ,get input and  printing  the best  5 complement * */
     std::string input;
+    CompletionView completionView;
     while(true){
         std::cout << "enter search\n ";
         std::getline (std::cin,input);//get input user
 //        std::cout<<"input "<<input<<std::endl;
         std::vector<AutoCompleteData> best_completions= GetBestKCompletions(input);
-        printAllCompletion(best_completions);
+//        printAllCompletion(best_completions);
+//        completionView.printToUser(best_completions);
+        completionView.printToUser(best_completions,offLineData);
     }
 }
 inline std::vector<AutoCompleteData> GoogleCompletion::GetBestKCompletions(const std::string& prefix){
