c7f7030d22043cceca3220a077a1de794d35847c
shoamco <shoamc98@gmail.com>
2019-12-10 22:51:46 -0800

    add test to open json file to jion object

--- TestAutoComplete.cpp
+++ TestAutoComplete.cpp
@@ -17,7 +17,7 @@ void testRunSearch() {
 /***************  Test Class   GoogleSearch  *************/
 void testCreateAutoCompleteData() {
     std::cout << "\n******test  testCreateAutoCompleteData *****\n\n" << std::endl;
-    AutoCompleteData autoCompleteData("Moses_and_the_Sages__Bibl",15750," And the saying pleased me well: and I took twelve men of you, one");
+    AutoCompleteData autoCompleteData("Moses_and_the_Sages__Bibl",15750," And the saying pleased me well: and I took twelve men of you, one",28);
     std::cout<<autoCompleteData;
 
 }
@@ -31,4 +31,28 @@ void test_init_offline_data() {
     printVectorCompletion(vectorCompletion);
 
 
+
+}
+
+void test_open_json_file(){
+    //    std::ifstream file("offline_result.json");
+////    json j;
+//    json data_json;
+//    file >> data_json;
+//std::string file_path("offline_result.json");
+//std::string file_name("outputWilliam_Shakespeare__Hamlet.txt");
+    std::string file_name("../profile.json");
+//    std::string file_name("offline_result.json");
+//    std::ifstream ifs{"offline_result.json"};
+
+
+    std::ifstream file(file_name);
+    if (file.is_open()) {
+        json data_json = json::parse(file);
+        for (json::iterator it = data_json.begin(); it != data_json.end(); ++it) {
+            std::cout << it.key() << " : " << it.value() << std::endl;
+        }
+    }
+    else
+    { throw "Unable to open file";}
 }
--- TestAutoComplete.h
+++ TestAutoComplete.h
@@ -9,9 +9,16 @@
 #include "AutoCompleteData.h"
 #include "OffLineData.h"
 #include <string>
-
+#include <json.hpp>
+#include <fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+using json = nlohmann::json;
 void testRunSearch();
 void testCreateAutoCompleteData();
 void testCreateAutoCompleteData();
 void test_init_offline_data();
+void test_json_file();
+void test_open_json_file();
 #endif //AUTOCOMPLETEGOOGLE_TESTAUTOCOMPLETE_H
